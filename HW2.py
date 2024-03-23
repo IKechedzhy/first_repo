7a222018100bc66b223a78d64c192cf2221517ef
IKechedzhy <i.s.kechedzhy@icloud.com>
2024-03-23 16:28:12 +0200

    HW2

---
+++ HW2.py
@@ -0,0 +1,14 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+        if min < 1  or max > 1000 or min >= max or quantity < 1 or quantity > (max - min + 1) :
+                return []
+
+        winner_numbers = random.sample(range(min, max + 1), quantity)
+        return sorted(winner_numbers)
+
+
+result = get_numbers_ticket(1, 1000, 5)
+
+print(f"Ващі виграшні номери: {result}")
+    
