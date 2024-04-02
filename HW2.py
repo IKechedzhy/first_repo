265160f405026135606b012479689a805b73f04d
IKechedzhy <i.s.kechedzhy@icloud.com>
2024-04-02 16:31:57 +0300

    HW3

--- HW2.py
+++ HW2.py
@@ -1,7 +1,7 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-        if min < 1  or max > 1000 or min >= max or quantity < 1 or quantity > (max - min + 1) :
+        if min < 1  or max > 1000 or min >= max or quantity < 1 or quantity > 5 :
                 return []
 
         winner_numbers = random.sample(range(min, max + 1), quantity)
