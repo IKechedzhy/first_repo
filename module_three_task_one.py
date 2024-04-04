3b0e3f7cc88b8787894d6450f3e93f7635e5f168
IKechedzhy <i.s.kechedzhy@icloud.com>
2024-04-04 21:56:10 +0300

    fixed

--- module_three_task_one.py
+++ module_three_task_one.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-date_inp = input("Введіть дату у фоматі РРРР-ММ-ДД: ")
 
 def get_days_from_today(date):
     date_dt = datetime.strptime(date, "%Y-%m-%d")
@@ -8,7 +7,18 @@ def get_days_from_today(date):
     new_time = today_time.toordinal() - date_dt.toordinal()
     return new_time
 
-days_fr_today = get_days_from_today(date_inp)
-print(f"Різниця між датами дорівнює {days_fr_today} днів")
+while True:
+
+    date_inp = input("Введіть дату у фоматі РРРР-ММ-ДД: ")
+
+    try:
+
+        difference_days = get_days_from_today(date_inp)
+        print(f"Різниця між датами дорівнює {difference_days} днів")
+        break
+
+    except ValueError:
+        print(f"Невірний формат дати(Зразок: РРРР-ММ-ДД)")
     
+
     
--- module_three_task_two.py
+++ module_three_task_two.py
@@ -1,14 +1,14 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-        if min < 1  or max > 1000 or min >= max or quantity < 1 or quantity > 5 :
+        if min < 1  or max > 49 or min >= max or quantity < 1 or quantity > 6 :
                 return []
 
         winner_numbers = random.sample(range(min, max + 1), quantity)
         return sorted(winner_numbers)
 
 
-result = get_numbers_ticket(1, 1000, 5)
+result = get_numbers_ticket(1, 49, 6)
 
 print(f"Ващі виграшні номери: {result}")
     
