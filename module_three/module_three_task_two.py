5b8b470354725ba187ea05154a16d35db5fbdf8c
IKechedzhy <i.s.kechedzhy@icloud.com>
2024-04-06 19:45:56 +0300

    fixed

--- module_three/module_three_task_two.py
+++ module_three/module_three_task_two.py
@@ -1,14 +1,14 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-        if min < 1  or max > 1000 or min >= max or quantity < 1 or quantity > 1000 :
+        if min < 1  or max > 1000 or min >= max or quantity < 1 or quantity > (max - min +1) :
                 return []
 
         winner_numbers = random.sample(range(min, max + 1), quantity)
         return sorted(winner_numbers)
 
 
-result = get_numbers_ticket(1, 150, 9)
+result = get_numbers_ticket(10, 50, 49)
 
 print(f"Ващі виграшні номери: {result}")
     
